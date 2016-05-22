29c8acb185bc549f07a8feb50b7542aecb2cc001
Per-Olof Gatter <gatterick@Per-Olofs-MacBook-Air.local>
2016-05-23 01:25:03 +0200

    Added Xunit Test Framework

---
+++ test/blockracer.UnitTests/Models/Race_Test.cs
@@ -0,0 +1,11 @@
+using System;
+using Xunit;
+
+namespace blockracer.UnitTests.Models.Race
+{
+    public class RaceUnitTest {
+        public void CreateRaceAndCheckNotStartedStatus() {
+            Assert.False(false, "");   
+        }
+    }
+}
